bc6d155c05450de103a9592699e5f07202050c4a
FullTimeScam <418.gingin@gmail.com>
2024-06-19 14:00:57 +0900

    로그인 안풀리게 하는 방법!

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,48 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
 import { ethers } from "ethers";
 import { JsonRpcSigner } from "ethers";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
+
+// 로그인 하고 새로고침했을 때 로그인이 풀리지 않게 하는 방법
+// 로그인 정보를 로컬스토리지에 저장
+// 새로고침 했을 때 로컬스토리지에 정보가 있는지 확인
+// 정보가 있다면 그 정보를 사용해서 로그인
+// *로그인 버튼을 없애고 useEffect만 쓰면 로그인 버튼 없이 바로 로그인창이 뜨도록 할 수 있다.
 
 const App: FC = () => {
   const [signer, setSigner] = useState<JsonRpcSigner | null>(null);
 
+  const getSigner = async () => {
+    if (!window.ethereum) return;
+
+    const provider = new ethers.BrowserProvider(window.ethereum);
+
+    setSigner(await provider.getSigner());
+  };
+
   const onClickMetamask = async () => {
     try {
-      if (!window.ethereum) return;
+      getSigner();
 
-      const provider = new ethers.BrowserProvider(window.ethereum);
-
-      setSigner(await provider.getSigner());
+      localStorage.setItem("isLogin", "true");
     } catch (error) {
       console.error(error);
     }
   };
+
+  const onClickLogout = () => {
+    setSigner(null);
+
+    localStorage.removeItem("isLogin");
+  };
+
+  useEffect(() => {
+    const localIsLogin = localStorage.getItem("isLogin");
+
+    if (localIsLogin === "true") {
+      getSigner();
+    }
+  }, []);
 
   return (
     <Flex
@@ -28,7 +54,10 @@ const App: FC = () => {
       flexDir="column"
     >
       {signer ? (
-        <Text>{signer.address}</Text>
+        <>
+          <Text>{signer.address}</Text>
+          <Button onClick={onClickLogout}>로그아웃</Button>
+        </>
       ) : (
         <Button onClick={onClickMetamask}>🦊 로그인</Button>
       )}
